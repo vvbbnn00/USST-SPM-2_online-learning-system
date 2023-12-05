ce98593c050cafa3dc74c3de1baa50c24a7c177d
gfdfdxc <29912959+gfdfdxc@users.noreply.github.com>
2023-12-06 00:03:30 +0800

    修改404颜色

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -7,21 +7,18 @@ import { useRouter } from 'next/navigation'
 
 
 export default function NotFound() {
-    const { asPath } = useRouter()
     return (<>
         <title>SPM2 - Not Found</title>
         {/* https://stackoverflow.com/questions/72673561/tailwind-h-screen-container-is-header-amount-of-height-too-far-off-the-bottom */}
-        <div className="p-24 h-screen" style={{ backgroundColor: "#87CEEB" }}>
-            <font size={4} color="#FFFFFF">
+        <div className="p-36 h-screen">
+            <font size={4} color="#000000">
                 <p style={{ fontSize: "100pt", margin: 0, paddingBottom: "20pt" }}>:(</p>
                 <p style={{ fontSize: "20pt", margin: 0, padding: 0 }}>
                     你所请求的页面并不存在
-                    <br />
-                    你可以将下方的详细信息上报给我们
                 </p>
                 <br />
                 <Button color="default" variant="light">
-                    <font size={4} color="#FFFFFF"><Link href="/">返回首页</Link></font>
+                    <font size={4} color="#000000"><Link href="/">返回首页</Link></font>
                 </Button>
                 <br />
                 <br className="p-20"/>
@@ -30,7 +27,7 @@ export default function NotFound() {
                         <br />
                         错误详细信息
                         <br />
-                        网页路由: {asPath}
+                        ERROR CODE: 404
                     </p>
                 </div>
             </font>
