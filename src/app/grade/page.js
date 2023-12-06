4d7082ebacba4aba903ea68f22ddef3d63918d67
vvbbnn00 <vvbbnn00@foxmail.com>
2023-12-06 12:47:56 +0800

    增加成绩页面的登录检查

--- src/app/grade/page.js
+++ src/app/grade/page.js
@@ -3,8 +3,13 @@ import NavBarComp from "@/components/layout/navbar";
 import React from "react";
 import {getGradeList, getQueryExams} from "@/service/grade";
 import GradeList from "@/app/grade/grade-table";
+import {isLogin} from "@/utils/auth";
+import {redirect} from "next/navigation";
 
 export default async function Questions({searchParams}) {
+    if (!await isLogin()) {
+        return redirect('/login')
+    }
 
     const examList = await getQueryExams();
     const gradeList = await getGradeList(examList);
